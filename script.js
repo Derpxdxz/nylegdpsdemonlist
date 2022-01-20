695858afd6905fe556cbc75e87b632e17340bb2c
Faris Al-Waisi <77356296+Derpxdxz@users.noreply.github.com>
2022-01-20 12:47:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function welp() {
+  window.alert("The Demonlist is still under maintaince, Fixing the main list positions, etc.");
+  window.alert("You can go to the previous page now if you want, there is nothing here to do");
+
+}
